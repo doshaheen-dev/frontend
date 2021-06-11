a6c3788a91314c5d83424447833f704d875b0dbf
sheshadrishete108 <sheshadrishete@doshaheen.com>
2021-06-11 15:28:30 +0530

    Added firebase sign-up and explore page

---
+++ lib/constants/theme_colors.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+class ThemeColor {
+  static Color greenColor = Color(0xff00A699);
+  static Color redColor = Color(0xffFF5A5F);
+}
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio_management/constants/theme_colors.dart';
+import 'package:portfolio_management/screens/explore.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,8 +8,87 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _currentTab = 0;
+  List<Widget> _children = [
+    Explore(),
+    Center(
+      child: Text(
+        "Request",
+      ),
+    ),
+    Center(
+      child: Text("Chat"),
+    ),
+    Center(
+      child: Text("Profile"),
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Home"));
+    return Scaffold(
+      backgroundColor: Color(0XFFE7EBEE),
+      body: _children[_currentTab],
+      bottomNavigationBar: BottomNavigationBar(
+          elevation: 5,
+          selectedItemColor: ThemeColor.greenColor,
+          unselectedItemColor: Colors.grey[800],
+          type: BottomNavigationBarType.fixed,
+          currentIndex: _currentTab,
+          onTap: (int value) {
+            setState(() {
+              _currentTab = value;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.explore,
+                size: 30,
+              ),
+              tooltip: "Explore",
+              title: Text(
+                "Explore",
+                style: TextStyle(fontSize: 11),
+              ),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.request_quote,
+                //color: Colors.black,
+                size: 30,
+              ),
+              tooltip: "Requests",
+              title: Text(
+                "Requests",
+                style: TextStyle(fontSize: 11),
+              ),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.question_answer,
+                //color: Colors.black,
+                size: 30,
+              ),
+              tooltip: "Chat",
+              title: Text(
+                "Chat",
+                style: TextStyle(fontSize: 11),
+              ),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.account_circle,
+                //color: Colors.black,
+                size: 30,
+              ),
+              tooltip: "Profile",
+              title: Text(
+                "Profile",
+                style: TextStyle(fontSize: 11),
+              ),
+            )
+          ]),
+    );
   }
 }
