4e35d2bff2b192372964720df56000c94196075e
Prachi Dighe <prachidighe@doshaheen.com>
2021-09-09 20:11:28 +0530

    Implemented Investor/Fundraiser FAQ

--- lib/screens/fundraiser/dashboard/fundraiser_faq.dart
+++ lib/screens/fundraiser/dashboard/fundraiser_faq.dart
@@ -1,6 +1,7 @@
 import 'package:acc/screens/common/webview_container.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class FundraiserFaq extends StatefulWidget {
   @override
@@ -8,11 +9,49 @@ class FundraiserFaq extends StatefulWidget {
 }
 
 class _FundraiserFaqState extends State<FundraiserFaq> {
+  final _key = UniqueKey();
+  bool isPageLoaded = true;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.yellowAccent,
-      child: WebViewContainer(""),
-    );
+    return Scaffold(
+        backgroundColor: Color(0xffffffff),
+        appBar: AppBar(
+          toolbarHeight: 0,
+          elevation: 0.0,
+          backgroundColor: (Color(0xffffffff)),
+        ),
+        body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Expanded(
+              child: Stack(children: [
+            Visibility(
+                visible: isPageLoaded,
+                child: Container(
+                  child: Center(
+                      child: CircularProgressIndicator(
+                    color: Theme.of(context).primaryColor,
+                  )),
+                )),
+            Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                child: WebView(
+                  key: _key,
+                  javascriptMode: JavascriptMode.unrestricted,
+                  initialUrl:
+                      "http://ami-corp-admin-portal.s3-website.ap-south-1.amazonaws.com/onboarding/fundraiser-faq",
+                  onPageStarted: (value) {
+                    setState(() {
+                      isPageLoaded = true;
+                    });
+                  },
+                  onPageFinished: (value) {
+                    setState(() {
+                      isPageLoaded = false;
+                    });
+                  },
+                ))
+          ]))
+        ]));
   }
 }
--- lib/screens/investor/dashboard/investor_faq.dart
+++ lib/screens/investor/dashboard/investor_faq.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class InvestorFaq extends StatefulWidget {
   @override
@@ -7,10 +8,49 @@ class InvestorFaq extends StatefulWidget {
 }
 
 class _InvestorFaqState extends State<InvestorFaq> {
+  final _key = UniqueKey();
+  bool isPageLoaded = true;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.yellowAccent,
-    );
+    return Scaffold(
+        backgroundColor: Color(0xffffffff),
+        appBar: AppBar(
+          toolbarHeight: 0,
+          elevation: 0.0,
+          backgroundColor: (Color(0xffffffff)),
+        ),
+        body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Expanded(
+              child: Stack(children: [
+            Visibility(
+                visible: isPageLoaded,
+                child: Container(
+                  child: Center(
+                      child: CircularProgressIndicator(
+                    color: Theme.of(context).primaryColor,
+                  )),
+                )),
+            Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                child: WebView(
+                  key: _key,
+                  javascriptMode: JavascriptMode.unrestricted,
+                  initialUrl:
+                      "http://ami-corp-admin-portal.s3-website.ap-south-1.amazonaws.com/onboarding/investor-faq",
+                  onPageStarted: (value) {
+                    setState(() {
+                      isPageLoaded = true;
+                    });
+                  },
+                  onPageFinished: (value) {
+                    setState(() {
+                      isPageLoaded = false;
+                    });
+                  },
+                ))
+          ]))
+        ]));
   }
 }
